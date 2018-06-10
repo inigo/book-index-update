b9cb8bc8e7dc9a8213527d8a5a85f8a7b132a4f7
Inigo <inigo.surguy@gmail.com>
2018-06-10 14:32:58 +0100

    Find matching contexts

--- src/main/scala/net/surguy/reindexer/PageMatcher.scala
+++ src/main/scala/net/surguy/reindexer/PageMatcher.scala
@@ -6,6 +6,9 @@ package net.surguy.reindexer
 class PageMatcher(oldVersion: List[Page], newVersion: List[Page]) {
   private val CONTEXT_SIZE_IN_WORDS = 5
   private val MAX_DISTANCE_FROM_ORIGINAL_PAGE = 4
+  private val CONTEXT_MATCH_THRESHOLD = 0.7
+
+  type Context = List[String]
 
   def findNewIndex(indexTerm: String, originalPageNumber: Int): Option[Int] = {
     val originalPage = oldVersion.find(_.pageNumber == originalPageNumber)
@@ -17,17 +20,19 @@ class PageMatcher(oldVersion: List[Page], newVersion: List[Page]) {
   }
 
   /** Return the words in the page close to the original term, for example the full sentence it appears in */
-  private[reindexer] def findContext(term: String, pageText: String, contextSize: Int = CONTEXT_SIZE_IN_WORDS): List[String] = {
+  private[reindexer] def findContext(term: String, pageText: String, contextSize: Int = CONTEXT_SIZE_IN_WORDS): Context = {
     import WordUtils.StringWithBreaks
     val words = pageText.words.toList
-    val position: Int = words.indexWhere(word => word.toLowerCase()==term.toLowerCase)
-    words.slice(position - contextSize, position + contextSize + 1)
+    val position: Int = words.indexWhere(_.toLowerCase==term.toLowerCase)
+    nearbyWords(words, position, contextSize)
   }
 
+  private def nearbyWords(words: List[String], position: Int, contextSize: Int): Context = words.slice(position - contextSize, position + contextSize + 1)
+
   /** Find the page that a term is on. */
-  private[reindexer] def findMatchingPage(indexTerm: String, context: List[String], startSearchAt: Int, pages: List[Page]): Option[Int] = {
+  private[reindexer] def findMatchingPage(indexTerm: String, context: Context, startSearchAt: Int, pages: List[Page]): Option[Int] = {
     val pagesToCheck: Seq[Int] = identifyPagesToCheck(startSearchAt, pages.length)
-    pagesToCheck.find(i => checkPage(pages(i).contents, indexTerm, context))
+    pagesToCheck.find(i => checkPage(pages(i).contents, indexTerm, context).isDefined )
   }
 
   /** Return a list of the pages to check, in order of likelihood - for example 6, 7, 5, 8, 4 if the term was on page 6 originally. */
@@ -38,7 +43,21 @@ class PageMatcher(oldVersion: List[Page], newVersion: List[Page]) {
   }
 
   /** Identify if a term is on a page, with a close-enough match for the surrounding context.  */
-  private[reindexer] def checkPage(contents: String, indexTerm: String, context: List[String]): Boolean = ???
+  private[reindexer] def checkPage(contents: String, indexTerm: String, context: Context, contextSize: Int = CONTEXT_SIZE_IN_WORDS): Option[List[String]] = {
+    import WordUtils.StringWithBreaks
+    val sanitizedContext = context.map(_.toLowerCase.replaceAll("[^A-Za-z0-9]",""))
+    val words = contents.words.toList.map(_.toLowerCase)
 
+    val potentialMatchIndices = words.zipWithIndex.filter(_._1 == indexTerm.toLowerCase).map(_._2)
+    val potentialContexts: Seq[Context] = potentialMatchIndices.map(i => nearbyWords(words, i, contextSize))
+    val foundMatch: Option[Context] = potentialContexts.find(pc => isCloseMatch(sanitizedContext, pc))
+    foundMatch
+  }
+
+  private def isCloseMatch(first: Context, second: Context):Boolean = {
+    val totalWords = Math.min(first.length, second.length)
+    val matchingWords = first.intersect(second).length
+    (matchingWords.toFloat / totalWords.toFloat) >= CONTEXT_MATCH_THRESHOLD
+  }
 
 }
--- src/test/scala/net/surguy/reindexer/PageMatcherSpec.scala
+++ src/test/scala/net/surguy/reindexer/PageMatcherSpec.scala
@@ -19,12 +19,26 @@ class PageMatcherSpec extends Specification {
   "identifying context" should {
     import pageMatcher.findContext
     val text = "The quick brown fox jumps over the lazy dog. It sings the song of happy chickens then the fox eats all the tasty hens. Om nom."
-    def split(s: String) = s.split(" ").toList
     "identify nearby words" in { findContext("jumps", text, 3) mustEqual split("quick brown fox jumps over the lazy") }
     "identify nearby words regardless of term case" in { findContext("Jumps", text, 3) mustEqual split("quick brown fox jumps over the lazy") }
     "ignore punctuation" in { findContext("dog", text, 3) mustEqual split("over the lazy dog It sings the") }
     "stop at the beginning of a page" in { findContext("quick", text, 3) mustEqual split("The quick brown fox jumps") }
     "stop at the end of a page" in { findContext("Om", text, 3) mustEqual split("the tasty hens Om nom") }
   }
+
+  "finding matches" should {
+    import pageMatcher.checkPage
+    val text = "The quick brown fox jumps over the lazy dog. It sings the song of happy chickens then the fox eats all the tasty hens. Om nom."
+    "find an exact match" in { checkPage(text, "jumps", split("quick brown fox jumps over the lazy")) must beSome }
+    "find an match with changed case" in { checkPage(text, "Jumps", split("quick Brown fox Jumps over the LAZY")) must beSome }
+    "find an match with additional words" in { checkPage(text, "jumps", split("quickish brown fox also jumps over the lazy")) must beSome }
+    "find an match with changed punctuation" in { checkPage(text, "jumps", split("quick brown fox jumps. Over the lazy!")) must beSome }
+    "reject a match with missing term" in { checkPage(text, "rabbit", split("quick brown rabbit jumps over the lazy")) must beNone }
+    "reject a match with changed context" in { checkPage(text, "jumps", split("my shiny horse jumps the fence regularly")) must beNone }
+    "find the first match if context is correct" in { checkPage(text, "fox", split("The quick brown fox jumps over the")) must beSome }
+    "find the second match if context is correct" in { checkPage(text, "fox", split("chickens then the fox eats all the tasty")) must beSome }
+  }
+
+  private def split(s: String) = s.split(" ").toList
 
 }
